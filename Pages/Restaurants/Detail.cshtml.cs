c5b9ff78b638d974b954aa241e15e816f96b8eb9
Jon <jsmartware@aol.com>
2020-11-01 22:21:45 -0800

    Added Detail NotFound page

--- Pages/Restaurants/Detail.cshtml.cs
+++ Pages/Restaurants/Detail.cshtml.cs
@@ -5,17 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
+        private readonly IRestaurantData restaurantData;
+
         //output model
         public Restaurant Restaurant { get; set; }
-        public void OnGet(int restaurantId)
+        public DetailModel(IRestaurantData restaurantData )
         {
-            Restaurant = new Restaurant(); // otherwise Detail Page gets Null Reference Exception.
-            Restaurant.Id = restaurantId;
-        }   
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page(); // renders Detail.cshtml
+        }    
     }
 }
