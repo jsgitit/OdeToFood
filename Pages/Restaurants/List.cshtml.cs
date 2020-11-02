382717c13a3ffde4d5758850010e1ebc6eeede66
Jon <jsmartware@aol.com>
2020-11-01 21:12:38 -0800

    Created Restaurant Search

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -17,16 +17,23 @@ namespace OdeToFood.Pages.Restaurants
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet = true)] // allows searchTerm to be preserved on the form.
+        public string SearchTerm { get; set; }
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
 
         {
             this.config = config;
             this.restaurantData = restaurantData;
         }
-        public void OnGet()
+        public void OnGet(string searchTerm) // "searchTerm" matches name in Request from search form.
+                                                // then it model binds to the Request.searchTerm value
+                                                // but page refreshes will pass in a null, which is ok for string
         {
+            
+
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantByName(SearchTerm);
         }
     }
 }
