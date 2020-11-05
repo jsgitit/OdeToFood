e469b0dd231d1087951318f9968a997f1c01b14f
Jon <jsmartware@aol.com>
2020-11-05 12:35:57 -0800

    Installed EF core, and added DBContext in OdeToFood.Data, added connectionstring, and service configuration

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OdeToFood.Core;
 using OdeToFood.Data;
 
 namespace OdeToFood
@@ -24,6 +26,11 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<OdeToFoodDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
             services.AddRazorPages();
         }
